f4dcee2e3d9757461e756df314a7980199a4f0be
Nick Plante <nap@zerosum.org>
2024-03-25 15:59:39 -0400

    add color names

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const ntc = require('ntcjs');
+
 function randomColor() {
     return '#' + Math.random().toString(16).substr(-6);
 }
 
-module.exports = randomColor;
+function randomColorWithName() {
+    const color = randomColor();
+    return {
+        color,
+        name: ntc.name(color)[1]
+    };
+}
+
+module.exports = {
+    randomColor,
+    randomColorWithName
+};
